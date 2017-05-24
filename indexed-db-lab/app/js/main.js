2c04d1545259d55dc2138ed7823dd3ed3609b3a5
jmtalarn <joanmaria.talarn@idiada.com>
2017-05-24 14:42:23 +0200

    5.2 Add sample orders

--- indexed-db-lab/app/js/main.js
+++ indexed-db-lab/app/js/main.js
@@ -243,7 +243,48 @@ var idbApp = (function() {
 
   function addOrders() {
 
-    // TODO 5.2 - add items to the 'orders' object store
+    dbPromise.then(function(db) {
+      var tx = db.transaction('orders', 'readwrite');
+      var store = tx.objectStore('orders');
+      var items = [{
+          name: 'Cabinet',
+          id: 'ca-brn-ma',
+          price: 799.99,
+          color: 'brown',
+          material: 'mahogany',
+          description: 'An intricately-designed, antique cabinet',
+          quantity: 7
+        },
+        {
+          name: 'Armchair',
+          id: 'ac-gr-pin',
+          price: 299.99,
+          color: 'grey',
+          material: 'pine',
+          description: 'A plush recliner armchair',
+          quantity: 3
+        },
+        {
+          name: 'Couch',
+          id: 'cch-blk-ma',
+          price: 499.99,
+          color: 'black',
+          material: 'mahogany',
+          description: 'A very comfy couch',
+          quantity: 3
+        }
+      ];
+      items.forEach(function(item) {
+        console.log('Adding item: ', item);
+        store.add(item);
+      });
+      return tx.complete;
+    }).then(function() {
+      console.log('All items added successfully!');
+    }).catch(function(e) {
+      console.log('Error adding items: ', e);
+    });
+
 
   }
 
