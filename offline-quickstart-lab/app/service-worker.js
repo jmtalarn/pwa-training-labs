1a6b8002767f6927046e1d0da2822013fbef6bd4
jmtalarn <joanmaria.talarn@idiada.com>
2017-06-07 14:50:47 +0200

    2.1 Cache static assets on install

--- offline-quickstart-lab/app/service-worker.js
+++ offline-quickstart-lab/app/service-worker.js
@@ -1,7 +1,22 @@
 (function() {
   'use strict';
 
-  // TODO 2.1 - Cache static assets on install
+  var CACHE_NAME = 'static-cache';
+
+  var urlsToCache = [
+    '.',
+    'index.html',
+    'styles/main.css'
+  ];
+
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
 
   // TODO 2.2 - Fetch from the cache
 
