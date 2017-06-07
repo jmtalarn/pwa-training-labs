42e0ae3121862d2b0c1cf8e0d65b886e669dbca9
jmtalarn <joanmaria.talarn@idiada.com>
2017-06-07 14:51:49 +0200

    2.2 Fetch from the cache

--- offline-quickstart-lab/app/service-worker.js
+++ offline-quickstart-lab/app/service-worker.js
@@ -18,6 +18,32 @@
     );
   });
 
-  // TODO 2.2 - Fetch from the cache
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+      .then(function(response) {
+        return response || fetchAndCache(event.request);
+      })
+    );
+  });
+
+  function fetchAndCache(url) {
+    return fetch(url)
+      .then(function(response) {
+        // Check if we received a valid response
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return caches.open(CACHE_NAME)
+          .then(function(cache) {
+            cache.put(url, response.clone());
+            return response;
+          });
+      })
+      .catch(function(error) {
+        console.log('Request failed:', error);
+        // You could return a custom offline 404 page here
+      });
+  }
 
 })();
