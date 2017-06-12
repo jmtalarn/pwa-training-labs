6abb858b7bab4da75c98bde5cc38adf1a62c7b69
jmtalarn <joanmaria.talarn@idiada.com>
2017-06-12 15:25:29 +0200

    3.8 Push the message from a Node.js server

--- push-notification-lab/app/node/main.js
+++ push-notification-lab/app/node/main.js
@@ -14,4 +14,30 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// TODO 3.8 - push a message using the web push library
+var webPush = require('web-push');
+
+var pushSubscription = {
+  "endpoint": "https://android.googleapis.com/gcm/send/dm9Bw4rILNc:APA91bGgVG2u0XR3fDvCXUGoS8efOmB-6leS6KeELqG9k--BliLXoLTbzFrJ3xwocWLLr-UxSzKkQOoP2fGeGiX3BcQTwXiYMLlrM41PohVqJRd8BHxP3c9C6rsCGwm6aIEZed3npJoz",
+  "keys": {
+    "p256dh": "BFDXHSkHjhd6VxmjU7zwN3r9yTxtzXQ8oAsQc-dplObA5bKFGKKx-Lc_Vz1Euqj9ThOF02xz9BN9HvXkr4TCSI8=",
+    "auth": "J55FYn4Csd4MDwBy3CEbag=="
+  }
+};
+
+// TODO 4.3a - include VAPID keys
+
+var payload = 'Here is a payload!';
+
+var options = {
+  gcmAPIKey: 'AAAA0RKzWf4:APA91bEQdHuS-HXeSsmArss7Q4IlOfH6bU3qSvZt2LMwnq7_uZorcJ1ZDAOtbWXzb1QQeEjo65fh1T0foTKb-B1Y08hvPvfPBIuNukECrbNypruaLC3c4SV-RyzxmUHCiWj64HSCCq_j',
+  TTL: 60,
+
+  // TODO 4.3b - add VAPID details
+
+};
+
+webPush.sendNotification(
+  pushSubscription,
+  payload,
+  options
+);
